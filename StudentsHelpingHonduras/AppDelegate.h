9a4fc7624d97feb548620eb78af74e6709619528
Brian Fallon <brianfallon@vpn-oncampus-89181.bu.edu>
2014-02-19 20:35:36 -0500

    Initial Commit

---
+++ StudentsHelpingHonduras/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StudentsHelpingHonduras
+//
+//  Created by Brian Fallon on 2/19/14.
+//  Copyright (c) 2014 Global App Initiative. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
